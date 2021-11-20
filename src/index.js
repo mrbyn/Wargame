8a6a79ba0fc94c10055ac350ae293024bb6be673
MrBIN <qlswhddn@naver.com>
2021-11-20 20:34:11 +0900

    Create React Project

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //app.js 의 내용을 index.html에 id가 root인 곳에 출력해라 라는 명령
 );
 
 // If you want to start measuring performance in your app, pass a function
